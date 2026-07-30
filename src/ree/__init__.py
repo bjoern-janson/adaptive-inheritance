0b4dee6dbdab3dd08684f24a4fda29a935b7625e
bjarki <bjoernjanson.de@gmail.com>
2026-07-30 16:08:22 +0200

    Create __init__.py

---
+++ src/ree/__init__.py
@@ -0,0 +1,12 @@
+"""
+Recursive Representation Expansion (REE) module.
+
+Provides gated representation expansion primitives for the
+Adaptive Inheritance framework.
+"""
+
+from .engine import REEEngine
+
+__all__ = [
+    "REEEngine",
+]
